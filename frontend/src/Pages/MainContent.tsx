08f7ec6894ae4d565286c5ce908c791e79bce4c7
piyush choudhary <piyuschoudahary.068@gmail.com>
2025-02-08 16:17:09 +0530

    make responsive

--- frontend/src/Pages/MainContent.tsx
+++ frontend/src/Pages/MainContent.tsx
@@ -64,7 +64,7 @@ export function MainContent() {
               setOpen(!open)
             }} user={user} />
          </div>
-         <div  className={ `absolute border-red-800   border-2 left-48   w-4/5 `}>
+         <div  className={ `absolute border-red-800   border-2 left-36 md:left-48    w-4/5 `}>
       <div>
           <AddContent isOpen={isOpen} onClose={()=>{
             setIsOpen(false)
