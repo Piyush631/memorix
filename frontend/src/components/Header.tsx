e7f662adc9194feb83c36a555a568e211bd71f96
piyush choudhary <piyuschoudahary.068@gmail.com>
2025-02-10 12:26:11 +0530

    change color

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -11,7 +11,7 @@ export function HeaderData({setIsOpen,setIsOpenShare}:head){
 
     return (
         <div >   
-             <div className={` ${!open ? "-ml-64":""}  flex gap-8   md:justify-end justify-start  md:-translate-x-11 -translate-x-28  p-3 `}>
+             <div className={` ${!open ? "-ml-64":""}  flex gap-8   md:justify-end justify-start  md:-translate-x-6 -translate-x-24  p-2 `}>
                   <Button  onClick={()=>{
                     setIsOpenShare(true)
                   }} text="Share Brain "  variant="secondary" startIcon={<Shareicon size="md" />}/>
