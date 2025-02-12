1442fb0383f79d5af74640a1d4841770429b714f
piyush choudhary <piyushchoudhary.068@gmail.com>
2025-02-12 16:40:10 +0530

    add search bar

--- frontend/src/Pages/LoadingPage.tsx
+++ frontend/src/Pages/LoadingPage.tsx
@@ -2,7 +2,7 @@ import { Loading } from "../components/Loading";
 
 export function LoadingPage() {
   return (
-    <div className="flex flex-wrap gap-12">
+    <div className="flex flex-wrap gap-8">
       {[...Array(6)].map((_, i) => (
         <Loading key={i} />
       ))}
--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -2,7 +2,7 @@
 export function Loading(){
     return (
         <div>
-            <div className="h-80 w-60  animate-pulse  rounded-xl p-1 m-4 border-2 border-gray-200 flex flex-col   gap-2">
+            <div className="h-80 w-56  animate-pulse  rounded-xl p-1 m-3 border-2 border-gray-200 flex flex-col   gap-2">
             <div className="h-6 w-3/4  bg-gray-800/20  rounded-xl"></div>
                 <div className="h-7 w-1/2 mt-2 bg-gray-800/20 rounded-xl"></div>
                 <div className="h-56 w-full  bg-gray-800/20   rounded-xl">
