463e6734cfaa8f977a7ca946d17ac6902ce41c39
piyush choudhary <piyushchoudhary.068@gmail.com>
2025-02-13 08:50:44 +0530

    create responsive

--- frontend/src/Pages/LoadingPage.tsx
+++ frontend/src/Pages/LoadingPage.tsx
@@ -3,7 +3,7 @@ import { Loading } from "../components/Loading";
 export function LoadingPage() {
   return (
     <div className="flex flex-wrap gap-8">
-      {[...Array(6)].map((_, i) => (
+      {[...Array(4)].map((_, i) => (
         <Loading key={i} />
       ))}
     </div>
