57872abb9cd64a1d3fa36a575ac68bfcc2c2119a
piyush choudhary <piyushchoudhary.068@gmail.com>
2025-02-13 09:23:33 +0530

    create responsive

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -27,7 +27,8 @@ export function HeaderData({setIsOpen,setIsOpenShare}:head){
   },[searchquery])
 
     return (
-        <div className="flex  justify-center md:justify-between px-16  " > 
+ 
+           <div className="flex flex-col md:flex-row justify-between px-16  " > 
           <div className="hidden md:flex ml-2  items-center gap-2">
                     <BrainLogo />
                     <div className="hidden md:block text-2xl font-semibold">
@@ -40,8 +41,8 @@ export function HeaderData({setIsOpen,setIsOpenShare}:head){
 <div className="hidden lg:flex">
 <input type="text" onChange={(e)=>{
   setSearchQuery(e.target.value)
-}} className="  p-2 h-8 w-64 bg-white rounded-md" placeholder="Search..."/>
-<div className=" absolute text-xl  top-4  right-[350px] "><CiSearch />
+}} className="  p-2 h-8 w-56 lg:w-64 bg-white rounded-md" placeholder="Search..."/>
+<div className=" hidden md:block  absolute text-xl  top-4  right-[350px] "><CiSearch />
 </div>
 
 
@@ -50,12 +51,13 @@ export function HeaderData({setIsOpen,setIsOpenShare}:head){
 
 
 </div>
+<div className="flex gap-2">
                 <Button 
                    onClick={()=>{
                     setIsOpenShare(true)  }} text="Share Brain "  variant="secondary" startIcon={<Shareicon size="md" />}/>
                 <Button   onClick={()=>{setIsOpen(true)}}  text="Add Content"  variant="primary" startIcon={  <PlusIcon size="md"/>}/>
              
-            
+            </div>
                   </div>
         </div>
     )
